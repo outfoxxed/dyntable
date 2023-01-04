818dde13c14b43dc7167614c4fe7d3de5b8850b6
outfoxxed <outfoxxed@outfoxxed.me>
2023-01-03 16:12:56 -0800

    Improve Send/Sync test coverage
    Fix Send/Sync test
    Add !Send/!Sync test

--- tests/send_sync.rs
+++ tests/send_sync.rs
@@ -24,9 +24,9 @@ trait UnboundedTrait {}
 struct Unbounded;
 impl UnboundedTrait for Unbounded {}
 
-fn require_send<T: Send>(_: &T) {}
-fn require_sync<T: Sync>(_: &T) {}
-fn require_send_sync<T: Send + Sync>(_: &T) {}
+fn require_send<T: Send>(_: T) {}
+fn require_sync<T: Sync>(_: T) {}
+fn require_send_sync<T: Send + Sync>(_: T) {}
 
 #[test]
 fn test_bounded() {
@@ -34,9 +34,13 @@ fn test_bounded() {
 	let sync = DynBox::<dyn TSync>::new(SSync);
 	let send_sync = DynBox::<dyn TSendSync>::new(SSendSync);
 
-	require_send(&send);
+	require_send(&sync);
 	require_sync(&sync);
 	require_send_sync(&send_sync);
+
+	require_send(send);
+	require_sync(sync);
+	require_send_sync(send_sync);
 }
 
 #[test]
@@ -45,7 +49,11 @@ fn test_added() {
 	let sync = DynBox::<dyn UnboundedTrait + Sync>::new(Unbounded);
 	let send_sync = DynBox::<dyn UnboundedTrait + Send + Sync>::new(Unbounded);
 
-	require_send(&send);
-	require_sync(&sync);
-	require_send_sync(&send_sync);
+	require_send(&*sync);
+	require_sync(&*sync);
+	require_send_sync(&*send_sync);
+
+	require_send(send);
+	require_sync(sync);
+	require_send_sync(send_sync);
 }
