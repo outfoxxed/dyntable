bc975d06f29c74be1d7887ea86dcbca5c0d09690
outfoxxed <outfoxxed@outfoxxed.me>
2023-02-17 01:18:47 -0800

    Add test for creation of an owned DynPtr in C

--- tests/ffi.c
+++ tests/ffi.c
@@ -1,4 +1,17 @@
+#include <stdlib.h>
 #include <stddef.h>
+
+static struct DebugFlags {
+	unsigned int cdealloc_calls;
+	unsigned int cdrop_calls;
+} debug_flags = {
+	.cdealloc_calls = 0,
+	.cdrop_calls = 0,
+};
+
+struct DebugFlags* get_debug_flags() {
+	return &debug_flags;
+}
 
 struct Layout {
 	size_t size;
@@ -22,6 +35,59 @@ struct DynPtr {
 	void* ptr;
 	void* vtable;
 };
+
+struct CValue {
+	int value;
+};
+
+void drop_c_value() {
+	debug_flags.cdrop_calls += 1;
+}
+
+int c_value_get(void* value) {
+	struct CValue* v = value;
+	return v->value;
+}
+
+void c_value_set(void* value, int set) {
+	struct CValue* v = value;
+	v->value = set;
+}
+
+static struct BoundedTraitVTable c_value_vtable = {
+	.drop = &drop_c_value,
+	.layout = {
+		.size = sizeof(struct CValue),
+		.align = _Alignof(struct CValue),
+	},
+	.parent = {
+		.drop = &drop_c_value,
+		.layout = {
+			.size = sizeof(struct CValue),
+			.align = _Alignof(struct CValue),
+		},
+		.get = &c_value_get,
+	},
+	.set = &c_value_set,
+};
+
+struct DynPtr new_c_value() {
+	struct CValue* allocation = malloc(sizeof(struct CValue));
+	allocation->value = 0;
+
+	struct DynPtr ptr = {
+		.ptr = allocation,
+		.vtable = &c_value_vtable,
+	};
+
+	return ptr;
+}
+
+void dealloc_c_value(void* value) {
+	free(value);
+	debug_flags.cdealloc_calls += 1;
+}
+
 
 void increment_bounded(struct DynPtr ptr) {
 	struct BoundedTraitVTable* vtable = ptr.vtable;
--- tests/ffi.rs
+++ tests/ffi.rs
@@ -1,10 +1,11 @@
 use std::{
 	env,
+	ffi::c_void,
 	fs,
 	process::{Command, Stdio},
 };
 
-use dyntable::*;
+use dyntable::{alloc::Deallocator, *};
 
 #[test]
 fn ffi() {
@@ -26,6 +27,15 @@ fn ffi() {
 	unsafe {
 		let lib = libloading::Library::new("./target/ffitest/libffi.so").unwrap();
 
+		let debug_flags = lib
+			.get::<unsafe extern "C" fn() -> *mut DebugFlags>(b"get_debug_flags")
+			.unwrap()();
+		let new_c_value = lib
+			.get::<unsafe extern "C" fn() -> DynPtr<dyn BoundedTrait>>(b"new_c_value")
+			.unwrap();
+		let dealloc_c_value = lib
+			.get::<unsafe extern "C" fn(*mut c_void)>(b"dealloc_c_value")
+			.unwrap();
 		let c_increment_bounded = lib
 			.get::<unsafe extern "C" fn(DynRefMut<dyn BoundedTrait>)>(b"increment_bounded")
 			.unwrap();
@@ -41,7 +51,40 @@ fn ffi() {
 			rust_value.get(),
 			c_get_parent(DynRef::upcast(DynBox::borrow(&rust_value)))
 		);
+
+		#[derive(Copy, Clone)]
+		struct CDeallocator {
+			dealloc: unsafe extern "C" fn(*mut c_void),
+		}
+
+		let c_dealloc = CDeallocator {
+			dealloc: *dealloc_c_value,
+		};
+
+		impl Deallocator for CDeallocator {
+			unsafe fn deallocate(&self, ptr: std::ptr::NonNull<u8>, _: alloc::MemoryLayout) {
+				(self.dealloc)(ptr.as_ptr() as *mut c_void)
+			}
+		}
+
+		assert_eq!((*debug_flags).cdrop_calls, 0);
+		assert_eq!((*debug_flags).cdealloc_calls, 0);
+
+		let mut c_value = DynBox::from_raw_in(new_c_value(), c_dealloc);
+		assert_eq!(c_value.get(), 0);
+		c_value.set(c_value.get() + 1);
+		assert_eq!(c_value.get(), 1);
+		drop(c_value);
+
+		assert_eq!((*debug_flags).cdrop_calls, 1);
+		assert_eq!((*debug_flags).cdealloc_calls, 1);
 	}
+}
+
+#[repr(C)]
+struct DebugFlags {
+	cdealloc_calls: u32,
+	cdrop_calls: u32,
 }
 
 #[dyntable]
