747001643903f72c52dbc2a5927f05e5de95cd26
outfoxxed <outfoxxed@outfoxxed.me>
2023-03-15 00:41:40 -0700

    Fix send/sync tests

--- tests/compile/fail/send_sync.rs
+++ tests/compile/fail/send_sync.rs
@@ -1,17 +1,28 @@
 use dyntable::{DynBox, dyntable};
 
+fn main() {}
+
 #[dyntable]
 trait TestTrait {}
+
+#[dyntable]
+trait TestSend: Send {}
 
 fn require_send<T: Send>(_: T) {}
 fn require_sync<T: Sync>(_: T) {}
 
 struct Dummy;
 impl TestTrait for Dummy {}
+impl TestSend for Dummy {}
 
-fn main() {
-	let dynbox = DynBox::<dyn TestTrait>::new(Dummy);
+fn not_present() {
+	let mut dynbox = DynBox::<dyn TestTrait>::new(Dummy);
 	let dynbox2 = DynBox::<dyn TestTrait>::new(Dummy);
+
+	// DynMutRef<dyn TestTrait> should not be Send
+	require_send(DynBox::borrow_mut(&mut dynbox));
+	// DynMutRef<dyn TestTrait> should not be Sync
+	require_sync(DynBox::borrow_mut(&mut dynbox));
 
 	// DynRef<dyn TestTrait> should not be Send
 	require_send(DynBox::borrow(&dynbox));
@@ -22,4 +33,17 @@ fn main() {
 	require_send(dynbox);
 	// DynBox<dyn TestTrait> should not be Sync
 	require_sync(dynbox2);
+}
+
+fn send_not_sync() {
+	let mut dynbox = DynBox::<dyn TestSend>::new(Dummy);
+
+	// DynRef(Mut)<dyn TestSend> should not be Send/Sync, as TestSend is not Sync
+	require_send(DynBox::borrow_mut(&mut dynbox));
+	require_send(DynBox::borrow(&dynbox));
+	require_sync(DynBox::borrow_mut(&mut dynbox));
+	require_sync(DynBox::borrow(&dynbox));
+
+	// DynBox<dyn TestSend> should not be Sync
+	require_sync(dynbox);
 }
--- tests/send_sync.rs
+++ tests/send_sync.rs
@@ -25,35 +25,33 @@ struct Unbounded;
 impl UnboundedTrait for Unbounded {}
 
 fn require_send<T: Send>(_: T) {}
-//fn require_sync<T: Sync>(_: T) {}
-//fn require_send_sync<T: Send + Sync>(_: T) {}
+fn require_sync<T: Sync>(_: T) {}
+fn require_send_sync<T: Send + Sync>(_: T) {}
 
 #[test]
 fn test_bounded() {
 	let send = DynBox::<dyn TSend>::new(SSend);
-	//let sync = DynBox::<dyn TSync>::new(SSync);
-	//let send_sync = DynBox::<dyn TSendSync>::new(SSendSync);
+	let sync = DynBox::<dyn TSync>::new(SSync);
+	let send_sync = DynBox::<dyn TSendSync>::new(SSendSync);
 
-	//require_send(&sync);
-	//require_sync(&sync);
-	//require_send_sync(&send_sync);
+	require_sync(&sync);
+	require_send_sync(&send_sync);
 
 	require_send(send);
-	//require_sync(sync);
-	//require_send_sync(send_sync);
+	require_sync(sync);
+	require_send_sync(send_sync);
 }
 
 #[test]
 fn test_added() {
 	let send = DynBox::<dyn UnboundedTrait + Send>::new(Unbounded);
-	//let sync = DynBox::<dyn UnboundedTrait + Sync>::new(Unbounded);
-	//let send_sync = DynBox::<dyn UnboundedTrait + Send + Sync>::new(Unbounded);
+	let sync = DynBox::<dyn UnboundedTrait + Sync>::new(Unbounded);
+	let send_sync = DynBox::<dyn UnboundedTrait + Send + Sync>::new(Unbounded);
 
-	//require_send(sync.borrow());
-	//require_sync(sync.borrow());
-	//require_send_sync(send_sync.borrow());
+	require_sync(&sync);
+	require_send_sync(&send_sync);
 
 	require_send(send);
-	//require_sync(sync);
-	//require_send_sync(send_sync);
+	require_sync(sync);
+	require_send_sync(send_sync);
 }
