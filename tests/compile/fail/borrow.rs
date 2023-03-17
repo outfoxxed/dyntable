fa1baf1f8bbbd93a44c7bd8c3eff33f2c5966557
outfoxxed <outfoxxed@outfoxxed.me>
2023-03-16 23:08:32 -0700

    Fix borrow test

--- tests/compile/fail/borrow.rs
+++ tests/compile/fail/borrow.rs
@@ -6,19 +6,19 @@ fn main() {}
 trait TestTrait {}
 
 fn _double_mut_borrow(mut dynref: DynRefMut<dyn TestTrait>) {
-	let a = DynRefMut::borrow(&dynref);
+	let a = DynRefMut::borrow_mut(&mut dynref);
 	let b = DynRefMut::borrow_mut(&mut dynref);
 	let _ = (a, b);
 }
 
 fn _shared_mut_overlap1(mut dynref: DynRefMut<dyn TestTrait>) {
-	let a = DynRefMut::borrow(&dynref);
+	let a = DynRefMut::borrow(&dynref).clone();
 	let b = DynRefMut::borrow_mut(&mut dynref);
 	let _ = (a, b);
 }
 
 fn _shared_mut_overlap2(mut dynref: DynRefMut<dyn TestTrait>) {
-	let a = DynRefMut::borrow(&dynref);
-	let b = DynRefMut::borrow_mut(&mut dynref);
+	let a = DynRefMut::borrow_mut(&mut dynref);
+	let b = DynRefMut::borrow(&dynref).clone();
 	let _ = (a, b);
 }
